fa96747596af924c1e1600b847740547756731bb
Pangust01 <danpangust@gmail.com>
2024-03-27 17:05:04 -0500

    5 holas mundo

--- taller_git/aplicacion.java
+++ taller_git/aplicacion.java
@@ -3,5 +3,9 @@ package taller_git;
 public class aplicacion {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
     }
 }
